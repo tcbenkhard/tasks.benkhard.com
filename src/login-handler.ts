49ffdc95932169c425a2e3a7da2599c74130762e
Timo Benkhard <tcbenkhard@gmail.com>
2024-03-05 11:49:23 +0100

    Fix extracting header

--- src/login-handler.ts
+++ src/login-handler.ts
@@ -9,11 +9,11 @@ const authService = new AuthService(new DocumentClient(), 'RandomSigningKeyThatI
 const decode = (str: string):string => Buffer.from(str, 'base64').toString('binary');
 
 export const handler = wrap_handler(async (event: APIGatewayProxyEvent, context: Context) => {
-    const authHeader = event.headers?.Authorization
-    if(!authService) {
+    const authHeader = event.headers['Authorization']
+    if(!authHeader) {
         throw new InvalidCredentialsError()
     }
-    const decodedHeader = decode(authHeader!.substring('Basic '.length));
+    const decodedHeader = decode(authHeader.substring('Basic '.length));
     const headerValues = decodedHeader.split(':');
     return await authService.login({
         email: headerValues[0],

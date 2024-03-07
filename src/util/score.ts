6f043ba5c921e33ac699f59b0611a1177f6ce5e7
Timo Benkhard <tcbenkhard@gmail.com>
2024-03-07 20:54:49 +0100

    Improve score calculation

--- src/util/score.ts
+++ src/util/score.ts
@@ -7,9 +7,8 @@ export class Score {
         const now = DateTime.now()
         const daysSinceLastCompleted = now.diff(lastCompl, 'days').toObject().days || 0
         const totalDuration = DateTime.fromISO(task.dueDate).diff(DateTime.fromISO(task.lastCompleted), 'days').toObject().days || 0
-        const durationScore = daysSinceLastCompleted / totalDuration
+        const durationScore = 2 * daysSinceLastCompleted / totalDuration
         const priorityScore = 1 + (task.priority / 5)
-        console.log(`Calculating score: ${durationScore} DS * ${priorityScore} PS`)
-        return durationScore * priorityScore
+        return Math.pow(durationScore, priorityScore)
     }
 }
